b57efef6671fa4f9063ebf9f5c34b2669acf05e1
Ángel Adrián Martínez Bernardino <angeladrianmartinezbernardino@gmail.com>
2024-02-24 01:53:55 -0600

    Ya funciona.

--- P1.py
+++ P1.py
@@ -7,16 +7,17 @@ import numpy as np
 # De Ángel Adrián Martínez Bernardino.
 # Inicializa pygame y crea una ventana.
 pygame.init()
-display = (800, 600)
-pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+Pantalla = (800, 600)
+pygame.display.set_mode(Pantalla, DOUBLEBUF | OPENGL)
+pygame.display.set_caption("Función seno")
 # Establece el punto de vista de la cámara.
-gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+gluPerspective(45, (Pantalla[0] / Pantalla[1]), 0.1, 50.0)
 # Mueve la cámara hacia atrás para que podamos ver el objeto (la función seno).
 glTranslatef(0.0, 0.0, -5)
 
 
 # Función para dibujar los ejes x e y.
-def draw_axes():
+def Dibujar_ejes():
     glLineWidth(2.0)  # Hace los ejes un poco más gruesos.
     glBegin(GL_LINES)
     # Eje X
@@ -30,7 +31,7 @@ def draw_axes():
 
 
 # Función para dibujar la función seno.
-def draw_sine_wave():
+def Dibujar_onda_sinusoidal():
     glColor3f(1.0, 0.0, 0.0)  # Rojo.
     glLineWidth(2.0)  # Ajusta el grosor de la línea de la función seno.
     glBegin(GL_LINE_STRIP)
@@ -42,16 +43,16 @@ def draw_sine_wave():
 
 # Bucle principal.
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for Evento in pygame.event.get():
+        if Evento.type == pygame.QUIT:
             pygame.quit()
             quit()
     # Limpia la pantalla y establece el fondo en blanco.
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glClearColor(1, 1, 1, 1)
     # Dibuja los ejes y la función seno.
-    draw_axes()
-    draw_sine_wave()
+    Dibujar_ejes()
+    Dibujar_onda_sinusoidal()
     # Actualiza la pantalla.
     pygame.display.flip()
     pygame.time.wait(10)
