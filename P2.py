d8d3327924a52064dd71f95c6d27d9a94e4fd387
Ángel Adrián Martínez Bernardino <angeladrianmartinezbernardino@gmail.com>
2024-02-24 01:09:20 -0600

    Gato.

--- P2.py
+++ P2.py
@@ -18,11 +18,11 @@ pygame.display.set_caption("Juego del gato")
 
 # Definir el tamaño de las celdas y el tablero.
 Tamaño_celda = 100
-board = np.zeros((3, 3))
+Tablero = np.zeros((3, 3))
 
 
 # Función para dibujar la cuadrícula.
-def draw_grid():
+def Dibujar_cuadricula():
     for x in range(1, 3):
         pygame.draw.line(Pantalla, Negro, (x * Tamaño_celda, 0), (x * Tamaño_celda, Tamaño_ventana[1]), 2)
     for y in range(1, 3):
@@ -30,105 +30,105 @@ def draw_grid():
 
 
 # Función para dibujar la "x" y la "o".
-def draw_moves():
-    for row in range(3):
-        for col in range(3):
-            if board[row][col] == 1:
-                pygame.draw.line(Pantalla, Rojo, (col * Tamaño_celda + 25, row * Tamaño_celda + 25),
-                                 (col * Tamaño_celda + 75, row * Tamaño_celda + 75), 2)
-                pygame.draw.line(Pantalla, Rojo, (col * Tamaño_celda + 75, row * Tamaño_celda + 25),
-                                 (col * Tamaño_celda + 25, row * Tamaño_celda + 75), 2)
-            elif board[row][col] == 2:
-                pygame.draw.circle(Pantalla, Azul, (col * Tamaño_celda + 50, row * Tamaño_celda + 50), 37, 2)
+def Dibujar_movimientos():
+    for Fila in range(3):
+        for Columna in range(3):
+            if Tablero[Fila][Columna] == 1:
+                pygame.draw.line(Pantalla, Rojo, (Columna * Tamaño_celda + 25, Fila * Tamaño_celda + 25),
+                                 (Columna * Tamaño_celda + 75, Fila * Tamaño_celda + 75), 2)
+                pygame.draw.line(Pantalla, Rojo, (Columna * Tamaño_celda + 75, Fila * Tamaño_celda + 25),
+                                 (Columna * Tamaño_celda + 25, Fila * Tamaño_celda + 75), 2)
+            elif Tablero[Fila][Columna] == 2:
+                pygame.draw.circle(Pantalla, Azul, (Columna * Tamaño_celda + 50, Fila * Tamaño_celda + 50), 37, 2)
 
 
 # Función para manejar los movimientos.
-def make_move(position, player):
-    if position == 1:
-        row, col = 2, 0
-    elif position == 2:
-        row, col = 2, 1
-    elif position == 3:
-        row, col = 2, 2
-    elif position == 4:
-        row, col = 1, 0
-    elif position == 5:
-        row, col = 1, 1
-    elif position == 6:
-        row, col = 1, 2
-    elif position == 7:
-        row, col = 0, 0
-    elif position == 8:
-        row, col = 0, 1
-    elif position == 9:
-        row, col = 0, 2
+def Hacer_movimiento(Posicion, Jugador):
+    if Posicion == 1:
+        Fila, Columa = 2, 0
+    elif Posicion == 2:
+        Fila, Columa = 2, 1
+    elif Posicion == 3:
+        Fila, Columa = 2, 2
+    elif Posicion == 4:
+        Fila, Columa = 1, 0
+    elif Posicion == 5:
+        Fila, Columa = 1, 1
+    elif Posicion == 6:
+        Fila, Columa = 1, 2
+    elif Posicion == 7:
+        Fila, Columa = 0, 0
+    elif Posicion == 8:
+        Fila, Columa = 0, 1
+    elif Posicion == 9:
+        Fila, Columa = 0, 2
 
-    if board[row][col] == 0:
-        board[row][col] = player
+    if Tablero[Fila][Columa] == 0:
+        Tablero[Fila][Columa] = Jugador
 
 
 # Función para verificar el estado del juego.
-def check_win():
+def Verificar_ganador():
     # Verificar filas.
-    for row in range(3):
-        if board[row][0] == board[row][1] == board[row][2] != 0:
-            return board[row][0]
+    for Fila in range(3):
+        if Tablero[Fila][0] == Tablero[Fila][1] == Tablero[Fila][2] != 0:
+            return Tablero[Fila][0]
     # Verificar columnas.
-    for col in range(3):
-        if board[0][col] == board[1][col] == board[2][col] != 0:
-            return board[0][col]
+    for Columna in range(3):
+        if Tablero[0][Columna] == Tablero[1][Columna] == Tablero[2][Columna] != 0:
+            return Tablero[0][Columna]
     # Verificar diagonales.
-    if board[0][0] == board[1][1] == board[2][2] != 0:
-        return board[0][0]
-    if board[0][2] == board[1][1] == board[2][0] != 0:
-        return board[0][2]
+    if Tablero[0][0] == Tablero[1][1] == Tablero[2][2] != 0:
+        return Tablero[0][0]
+    if Tablero[0][2] == Tablero[1][1] == Tablero[2][0] != 0:
+        return Tablero[0][2]
     return 0
 
 
-def make_move_based_on_mouse(row, col, player):
-    board[row][col] = player
+def Mover_con_raton(Fila, Columna, Jugador):
+    Tablero[Fila][Columna] = Jugador
     pass
 
 
 # Bucle principal del juego.
-running = True
-current_player = 1
+Ejecucion = True
+Jugador_actual = 1
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouseX, mouseY = pygame.mouse.get_pos()  # Obtener la posición del clic.
+while Ejecucion:
+    for Evento in pygame.event.get():
+        if Evento.type == pygame.QUIT:
+            Ejecucion = False
+        if Evento.type == pygame.MOUSEBUTTONDOWN:
+            Raton_x, Raton_y = pygame.mouse.get_pos()  # Obtener la posición del clic.
 
-            clicked_row = int(mouseY // Tamaño_celda)
-            clicked_col = int(mouseX // Tamaño_celda)
+            Fila_clickeada = int(Raton_y // Tamaño_celda)
+            Columna_clickeada = int(Raton_x // Tamaño_celda)
 
             # Convertir la fila y columna clickeada a la posición en el tablero.
-            if board[clicked_row][clicked_col] == 0:  # Verificar si la celda está vacía
-                make_move_based_on_mouse(clicked_row, clicked_col, current_player)
-                current_player = 1 if current_player == 2 else 2
+            if Tablero[Fila_clickeada][Columna_clickeada] == 0:  # Verificar si la celda está vacía.
+                Mover_con_raton(Fila_clickeada, Columna_clickeada, Jugador_actual)
+                Jugador_actual = 1 if Jugador_actual == 2 else 2
 
     # Fondo de pantalla, dibujar la cuadrícula y movimientos, etcétera.
     Pantalla.fill(Blanco)
-    draw_grid()
-    draw_moves()
+    Dibujar_cuadricula()
+    Dibujar_movimientos()
 
     # Verificar si hay ganador.
-    winner = check_win()
-    if winner:
+    Ganador = Verificar_ganador()
+    if Ganador:
         # Después de detectar un ganador, pero antes del reinicio del tablero.
-        font = pygame.font.Font(None, 36)  # None usa la fuente predeterminada, 36 es el tamaño del texto.
-        text = font.render(f'Jugador {winner} gana!', True, Negro)
-        text_rect = text.get_rect(center=(Tamaño_ventana[0] / 2, Tamaño_ventana[1] / 2))
+        Fuente = pygame.font.Font(None, 36)  # None usa la fuente predeterminada, 36 es el tamaño del texto.
+        Texto = Fuente.render(f'Jugador {Ganador} gana!', True, Negro)
+        Texto_recto = Texto.get_rect(center=(Tamaño_ventana[0] / 2, Tamaño_ventana[1] / 2))
         Pantalla.fill(Blanco)  # Opcional, dependiendo de cómo quieras que se vea el mensaje.
-        Pantalla.blit(text, text_rect)
+        Pantalla.blit(Texto, Texto_recto)
         pygame.display.flip()
         time.sleep(5)  # Muestra el mensaje durante 5 segundos antes de reiniciar.
-        board = np.zeros((3, 3))  # Reiniciar el tablero.
+        Tablero = np.zeros((3, 3))  # Reiniciar el tablero.
 
     # Cambiar jugador.
-    current_player = 1 if current_player == 2 else 2
+    Jugador_actual = 1 if Jugador_actual == 2 else 2
 
     # Actualizar la pantalla.
     pygame.display.flip()
