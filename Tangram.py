7bf173022146928b2b2eb2d6481d521f2507c62d
Ángel Adrián Martínez Bernardino <angeladrianmartinezbernardino@gmail.com>
2024-10-11 09:42:01 -0600

    Tangram bien presentado.

--- Tangram.py
+++ Tangram.py
@@ -1,62 +1,50 @@
-# Practica 5 OpenGL tangram 2D
-# Mi primer programa OpenGL
 import pygame as pg
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
 class App:
-
     def __init__(self):
-        #inicilizar la ventana de pygame
         pg.init()
-                        #   tam vent    OPENGL    Buffer doble
         pg.display.set_mode((800,600),pg.OPENGL|pg.DOUBLEBUF)
         display = (800, 600)
         gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
         glTranslatef(0.0, 0.0, -2)
         pg.display.set_caption("Dibujar tangram 2D con OpenGL")
-        self.clock = pg.time.Clock() # controla el frame rate
-
-        #iniciar opengl
-        glClearColor(0.0, 0.0, 0.0, 1) #color de fondo
+        self.clock = pg.time.Clock()
+        glClearColor(0.0, 0.0, 0.0, 1)
         self.mainLoop()
-
 
     def mainLoop(self):
         rtri = 0.25
         running = True
         while (running):
-            #checar eventos
             for event in pg.event.get():
                 if (event.type == pg.QUIT):
                     running = False
-
-            #dibujar tangram 2D
             glClear(GL_COLOR_BUFFER_BIT)
             glMatrixMode(GL_MODELVIEW)
-
+            #Triángulo grande verde oscuro.
             glColor3f(0,0.5,0)
             glBegin(GL_TRIANGLES)
-            glVertex3f(0.0, 0.0, 0.0) # Top
-            glVertex3f(-0.5, 0.5, 0.0) # Bottom Left
-            glVertex3f(0.5, 0.5, 0.0) # Bottom right
+            glVertex3f(0.0, 0.0, 0.0)
+            glVertex3f(-0.5, 0.5, 0.0)
+            glVertex3f(0.5, 0.5, 0.0)
             glEnd()
-
-            #glLoadIdentity()
+            # Triángulo grande naranja claro.
             glColor3f(1, 0.25, 0)
             glBegin(GL_TRIANGLES)
-            glVertex3f(0.0, 0.0, 0.0)  # Top
-            glVertex3f(-0.5, -0.5, 0.0)  # Bottom Left
-            glVertex3f(-0.5, 0.5, 0.0)  # Bottom right
+            glVertex3f(0.0, 0.0, 0.0)
+            glVertex3f(-0.5, -0.5, 0.0)
+            glVertex3f(-0.5, 0.5, 0.0)
             glEnd()
-
+            # Triángulo pequeño rojo claro.
             glColor3f(1, 0, 0)
             glBegin(GL_TRIANGLES)
-            glVertex3f(0.25, 0.25, 0.0)  # Top
-            glVertex3f(0.5, 0.5, 0.0)  # Bottom Left
-            glVertex3f(0.5, 0.0, 0.0)  # Bottom right
+            glVertex3f(0.25, 0.25, 0.0)
+            glVertex3f(0.5, 0.5, 0.0)
+            glVertex3f(0.5, 0.0, 0.0)
             glEnd()
-
+            # Cuadrado amarillo claro.
             glColor3f(1, 1, 0)
             glBegin(GL_QUADS)
             glVertex3f(0.0, 0.0, 0.0)
@@ -64,14 +52,14 @@ class App:
             glVertex3f(0.5, 0.0, 0.0)
             glVertex3f(0.25, -0.25, 0.0)
             glEnd()
-
+            # Triángulo pequeño morado oscuro.
             glColor3f(0.5, 0, 1)
             glBegin(GL_TRIANGLES)
             glVertex3f(0.0, 0.0, 0.0)  # Top
             glVertex3f(0.25, -0.25, 0.0)  # Bottom Left
             glVertex3f(-0.25, -0.25, 0.0)  # Bottom right
             glEnd()
-
+            # Romboide azul oscuro.
             glColor3f(0, 0, 1)
             glBegin(GL_QUADS)
             glVertex3f(-0.25, -0.25, 0.0)
@@ -79,19 +67,16 @@ class App:
             glVertex3f(0.0, -0.5, 0.0)
             glVertex3f(-0.5, -0.5, 0.0)
             glEnd()
-
+            # Triángulo mediano rosa claro.
             glColor3f(1, 0, 1)
             glBegin(GL_TRIANGLES)
-            glVertex3f(0.5, 0.0, 0.0)  # Top
-            glVertex3f(0.5, -0.5, 0.0)  # Bottom Left
-            glVertex3f(0.0, -0.5, 0.0)  # Bottom right
+            glVertex3f(0.5, 0.0, 0.0)
+            glVertex3f(0.5, -0.5, 0.0)
+            glVertex3f(0.0, -0.5, 0.0)
             glEnd()
             glFlush()
-
-            #refrescar pantalla
+            # Refrescar pantalla.
             pg.display.flip()
-
-            #timing 60 cuadros por segundo
             self.clock.tick(60)
         self.quit()
 
